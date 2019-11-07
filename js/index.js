aecec88c4d5fce2d507ce597841d840cb9dc8bd7
Yashaswini <ysureshostb2@gmail.com>
2019-11-07 17:31:26 +0530

    second

--- js/index.js
+++ js/index.js
@@ -32,3 +32,23 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+var day = new Date();
+var hr = day.getHours();
+if (hr >= 0 && hr < 12) {
+   time();
+} else if (hr == 12) {
+    document.write("Good Afternoon!");
+} else if (hr >= 12 && hr <= 17) {
+    document.write("Good Evening!"+"<br>"+ " user");
+} else {
+    document.write("Good night !");
+}
+document.write();
+
+function time(){
+    document.write("Good Morning!");
+}
+function funboi()
+{
+  document.getElementById("pp").innerHTML = "";
+}
